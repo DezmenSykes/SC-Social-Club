32f36cdb3d4fafe0c1035f8f782c00a329b173f8
Dezmen Sykes <dezmensykes.ds1@gmail.com>
2024-11-07 20:29:55 -0600

    Added stories component.

--- src/app/components/Stories.tsx
+++ src/app/components/Stories.tsx
@@ -1,7 +1,166 @@
+import Image from "next/image";
 import React from "react";
 
 const Stories = () => {
-  return <div>Stories</div>;
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs">
+      <div className="flex gap-8 w-max">
+        { for (let i = 0; i < 10; i++) ()
+          <div className="flex flex-col items-center gap-2 cursor-pointer"></div>
+        }
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/22147574/pexels-photo-22147574/free-photo-of-two-girls-riding-bicycles-and-a-dog-running-on-a-countryside-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width={80}
+            height={80}
+            alt="Profile"
+            className="w-20 h-20 rounded-full ring"
+          />
+          <span className="font-medium">David</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/22147574/pexels-photo-22147574/free-photo-of-two-girls-riding-bicycles-and-a-dog-running-on-a-countryside-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width={80}
+            height={80}
+            alt="Profile"
+            className="w-20 h-20 rounded-full ring"
+          />
+          <span className="font-medium">David</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/22147574/pexels-photo-22147574/free-photo-of-two-girls-riding-bicycles-and-a-dog-running-on-a-countryside-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width={80}
+            height={80}
+            alt="Profile"
+            className="w-20 h-20 rounded-full ring"
+          />
+          <span className="font-medium">David</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/22147574/pexels-photo-22147574/free-photo-of-two-girls-riding-bicycles-and-a-dog-running-on-a-countryside-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width={80}
+            height={80}
+            alt="Profile"
+            className="w-20 h-20 rounded-full ring"
+          />
+          <span className="font-medium">David</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/22147574/pexels-photo-22147574/free-photo-of-two-girls-riding-bicycles-and-a-dog-running-on-a-countryside-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width={80}
+            height={80}
+            alt="Profile"
+            className="w-20 h-20 rounded-full ring"
+          />
+          <span className="font-medium">David</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/22147574/pexels-photo-22147574/free-photo-of-two-girls-riding-bicycles-and-a-dog-running-on-a-countryside-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width={80}
+            height={80}
+            alt="Profile"
+            className="w-20 h-20 rounded-full ring"
+          />
+          <span className="font-medium">David</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/22147574/pexels-photo-22147574/free-photo-of-two-girls-riding-bicycles-and-a-dog-running-on-a-countryside-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width={80}
+            height={80}
+            alt="Profile"
+            className="w-20 h-20 rounded-full ring"
+          />
+          <span className="font-medium">David</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/22147574/pexels-photo-22147574/free-photo-of-two-girls-riding-bicycles-and-a-dog-running-on-a-countryside-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width={80}
+            height={80}
+            alt="Profile"
+            className="w-20 h-20 rounded-full ring"
+          />
+          <span className="font-medium">David</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/22147574/pexels-photo-22147574/free-photo-of-two-girls-riding-bicycles-and-a-dog-running-on-a-countryside-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width={80}
+            height={80}
+            alt="Profile"
+            className="w-20 h-20 rounded-full ring"
+          />
+          <span className="font-medium">David</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/22147574/pexels-photo-22147574/free-photo-of-two-girls-riding-bicycles-and-a-dog-running-on-a-countryside-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width={80}
+            height={80}
+            alt="Profile"
+            className="w-20 h-20 rounded-full ring"
+          />
+          <span className="font-medium">David</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/22147574/pexels-photo-22147574/free-photo-of-two-girls-riding-bicycles-and-a-dog-running-on-a-countryside-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width={80}
+            height={80}
+            alt="Profile"
+            className="w-20 h-20 rounded-full ring"
+          />
+          <span className="font-medium">David</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/22147574/pexels-photo-22147574/free-photo-of-two-girls-riding-bicycles-and-a-dog-running-on-a-countryside-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width={80}
+            height={80}
+            alt="Profile"
+            className="w-20 h-20 rounded-full ring"
+          />
+          <span className="font-medium">David</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/22147574/pexels-photo-22147574/free-photo-of-two-girls-riding-bicycles-and-a-dog-running-on-a-countryside-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width={80}
+            height={80}
+            alt="Profile"
+            className="w-20 h-20 rounded-full ring"
+          />
+          <span className="font-medium">David</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/22147574/pexels-photo-22147574/free-photo-of-two-girls-riding-bicycles-and-a-dog-running-on-a-countryside-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width={80}
+            height={80}
+            alt="Profile"
+            className="w-20 h-20 rounded-full ring"
+          />
+          <span className="font-medium">David</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/22147574/pexels-photo-22147574/free-photo-of-two-girls-riding-bicycles-and-a-dog-running-on-a-countryside-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width={80}
+            height={80}
+            alt="Profile"
+            className="w-20 h-20 rounded-full ring"
+          />
+          <span className="font-medium">David</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Stories;
