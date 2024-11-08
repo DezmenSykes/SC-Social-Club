1b68403b677fbd02d14f40a048c71c4601de4847
Dezmen Sykes <dezmensykes.ds1@gmail.com>
2024-11-08 02:54:19 -0600

    Stories update..

--- src/app/components/Stories.tsx
+++ src/app/components/Stories.tsx
@@ -3,11 +3,8 @@ import React from "react";
 
 const Stories = () => {
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs">
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
       <div className="flex gap-8 w-max">
-        { for (let i = 0; i < 10; i++) ()
-          <div className="flex flex-col items-center gap-2 cursor-pointer"></div>
-        }
         <div className="flex flex-col items-center gap-2 cursor-pointer">
           <Image
             src="https://images.pexels.com/photos/22147574/pexels-photo-22147574/free-photo-of-two-girls-riding-bicycles-and-a-dog-running-on-a-countryside-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
