0a67200fe94e9569f1aec98ccd95814ac7dbc99c
Dezmen Sykes <dezmensykes.ds1@gmail.com>
2024-11-11 19:17:36 -0600

    Finished right sidebar including user panels.

--- src/app/components/UserInfoCard.tsx
+++ src/app/components/UserInfoCard.tsx
@@ -1,7 +1,69 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const UserInfoCard = () => {
-  return <div>UserInfoCard</div>;
+const UserInfoCard = ({ userId }: { userId: string }) => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
+      {/* Top */}
+      <div className="flex justify-between items-center font-medium">
+        <span className="text-gray-500">User Information</span>
+        <Link href="/" className="text-blue-500 text-xs">
+          See All
+        </Link>
+      </div>
+      <div className="flex flex-col gap-4 text-gray-500">
+        <div className="flex items-center gap-2">
+          <span className="text-xl text-black">Dezmen Sykes</span>
+          <span className="text-sm">@the.radical.son</span>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta fugit
+          molestias mollitia. Soluta fugit molestias mollitia Soluta fugit
+          molestias mollitia.
+        </p>
+        <div className="flex items-center gap-2">
+          <Image src="/map.png" alt="" width={16} height={16} />
+          <span>
+            Living in <b>St. Louis, MO </b>
+          </span>
+        </div>
+        <div className="flex items-center gap-2">
+          <Image src="/school.png" alt="" width={16} height={16} />
+          <span>
+            Went to <b>Harvard University</b>
+          </span>
+        </div>
+        <div className="flex items-center gap-2">
+          <Image src="/work.png" alt="" width={16} height={16} />
+          <span>
+            Works at <b>Sykes Creatives Ltd.</b>
+          </span>
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex gap-1 items-center">
+            <Image src="/link.png" alt="" width={16} height={16} />
+            <Link
+              href="https://sykescreatives.net"
+              className="text-blue-500 font-medium"
+            >
+              sykescreatives.net
+            </Link>
+          </div>
+          <div className="flex gap-1 items-center">
+            <Image src="/date.png" alt="" width={16} height={16} />
+            <span>Joined November 2024</span>
+          </div>
+        </div>
+        <button className="bg-blue-500 text-white text-sm rounded-md p-2">
+          Follow
+        </button>
+        <span className="text-red-400 self-end text-xs cursor-pointer">
+          Block User
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default UserInfoCard;
--- src/app/components/UserMediaCard.tsx
+++ src/app/components/UserMediaCard.tsx
@@ -1,7 +1,69 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const UserMediaCard = () => {
-  return <div>UserMediaCard</div>;
+const UserMediaCard = ({ userId }: { userId: string }) => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
+      {/* Top */}
+      <div className="flex justify-between items-center font-medium">
+        <span className="text-gray-500">User Media</span>
+        <Link href="/" className="text-blue-500 text-xs">
+          See All
+        </Link>
+      </div>
+      <div className="flex gap-4  flex-wrap">
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/2147868/pexels-photo-2147868.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            className="rounded-md object-cover"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/2147868/pexels-photo-2147868.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            className="rounded-md object-cover"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/2147868/pexels-photo-2147868.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            className="rounded-md object-cover"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/2147868/pexels-photo-2147868.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            className="rounded-md object-cover"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/2147868/pexels-photo-2147868.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            className="rounded-md object-cover"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/2147868/pexels-photo-2147868.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            className="rounded-md object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserMediaCard;
