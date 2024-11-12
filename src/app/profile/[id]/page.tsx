3023fe8b5b01497641e5ffa24766cf9f8563beb5
Dezmen Sykes <dezmensykes.ds1@gmail.com>
2024-11-11 20:06:12 -0600

    Finished profile page banner section.

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -18,8 +18,30 @@ const ProfilePage = () => {
                 src="https://images.pexels.com/photos/12206301/pexels-photo-12206301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                 alt=""
                 fill
-                className="object-cover"
+                className="object-cover rounded-md"
               />
+              <Image
+                src="https://images.pexels.com/photos/14552146/pexels-photo-14552146.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+                width={128}
+                height={128}
+                className="w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white z-10 object-cover"
+              />
+            </div>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">Dezmen Sykes</h1>
+            <div className="flex items-center gap-12 mb-4">
+              <div className="flex flex-col items-center">
+                <span className="font-medium">923</span>
+                <span className="text-sm">Posts</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">6.5K</span>
+                <span className="text-sm">Followers</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">1.8K</span>
+                <span className="text-sm">Following</span>
+              </div>
             </div>
           </div>
           <Feed />
